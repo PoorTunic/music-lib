4f53a4d1b5ade40af133cdf35c170cd934fb5801
Daniel Clemente <minedan1057@gmail.com>
2020-06-03 13:18:28 -0500

    Initial Commit

---
+++ src/com/uttec/database/Connection.java
@@ -0,0 +1,15 @@
+package com.uttec.database;
+
+/**
+ * 
+ * @author Daniel Clemente Aguirre, Daniela Hernández Hernández, Juan Alberto
+ *         Osorio Osorio
+ * @version 1.0
+ */
+public class Connection {
+
+	public Connection() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/uttec/entities/Artist.java
@@ -0,0 +1,14 @@
+package com.uttec.entities;
+
+/**
+ * 
+ * @author Daniel Clemente Aguirre, Daniela Hernández Hernández, Juan Alberto
+ *         Osorio Osorio
+ * @version 1.0
+ */
+public class Artist {
+
+	public Artist() {
+	}
+
+}
